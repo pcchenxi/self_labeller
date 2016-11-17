bb88eb6c57053c689c714905b5a7fc27eb311973
xi <xi8@kth.se>
2016-11-17 20:47:01 +0100

    first commit

---
+++ src/self_labeller.cpp
@@ -0,0 +1,290 @@
+#include "ros/ros.h"
+#include <tf/transform_listener.h>
+#include <sensor_msgs/point_cloud_conversion.h>
+#include "pcl_ros/point_cloud.h"
+#include <image_transport/image_transport.h>
+#include <sensor_msgs/CompressedImage.h>
+
+// image related
+#include <sensor_msgs/image_encodings.h>
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <cv_bridge/cv_bridge.h>
+
+// pointcloud related
+#include <sensor_msgs/PointCloud2.h>
+#include <sensor_msgs/point_cloud_conversion.h>
+#include <pcl_ros/transforms.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/filters/passthrough.h>
+ 
+#include "self_labeller.h" 
+ 
+using namespace std;  
+using namespace cv; 
+tf::TransformListener* tfListener = NULL;
+
+image_transport::Publisher pub_img;
+ros::Publisher  pub_cloud, pub_path;
+
+/////////////////////////////////////////////////////////////
+string input_frame_ = "base_link_oriented";
+string world_frame_ = "world_corrected";
+string base_frame_  = "base_link_oriented";
+string camera_frame_= "kinect2_rgb_optical_frame";
+
+vector<Eigen::Matrix4f> transforms_to_camera_;
+vector<Eigen::Matrix4f> transforms_to_world_;
+vector<Mat> raw_images_;
+
+pcl::PointCloud<pcl::PointXYZRGB> robot_path_;
+sensor_msgs::PointCloud2ConstPtr registered_cloud_;
+
+int count_ = 0;
+
+Cloud_Image_Mapper *ci_mapper;
+
+// sensor_msgs::ImageConstPtr img_raw_;
+// ros::Time cloud_in_time_; 
+
+void publish(ros::Publisher pub, pcl::PointCloud<pcl::PointXYZRGB> cloud, int type = 2)
+{
+    sensor_msgs::PointCloud2 pointlcoud2;
+    pcl::toROSMsg(cloud, pointlcoud2);
+
+    if(type == 2)
+    {
+        pub.publish(pointlcoud2);
+    }
+    else
+    {
+        sensor_msgs::PointCloud pointlcoud;
+        sensor_msgs::convertPointCloud2ToPointCloud(pointlcoud2, pointlcoud);
+
+        pointlcoud.header = pointlcoud2.header;
+        pub.publish(pointlcoud);
+    }
+}
+
+sensor_msgs::PointCloud2 transform_cloud(sensor_msgs::PointCloud2 cloud_in, string frame_target)
+{
+    ////////////////////////////////// transform ////////////////////////////////////////
+    sensor_msgs::PointCloud2 cloud_out;
+    tf::StampedTransform to_target;
+
+    try 
+    {
+        // tf_listener_->waitForTransform(frame_target, cloud_in.header.frame_id, cloud_in.header.stamp, ros::Duration(1.0));
+        tfListener->lookupTransform(frame_target, cloud_in.header.frame_id, cloud_in.header.stamp, to_target);
+        // tfListener->lookupTransform(frame_target, cloud_in.header.frame_id, ros::Time(0), to_target);
+    }
+    catch (tf::TransformException& ex) 
+    { 
+        ROS_WARN("[draw_frames] TF exception:\n%s", ex.what());
+        // return cloud_in;
+    }  
+
+    Eigen::Matrix4f eigen_transform;
+    pcl_ros::transformAsMatrix (to_target, eigen_transform);
+    pcl_ros::transformPointCloud (eigen_transform, cloud_in, cloud_out);
+
+    cloud_out.header.frame_id = frame_target;
+    return cloud_out;
+}
+
+pcl::PointCloud<pcl::PointXYZRGB> cloud_filter(pcl::PointCloud<pcl::PointXYZRGB> cloud)
+{
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr  input_cloud       (new pcl::PointCloud<pcl::PointXYZRGB>(cloud));
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr  cloud_passthrough (new pcl::PointCloud<pcl::PointXYZRGB>);
+
+    pcl::PassThrough<pcl::PointXYZRGB> pass;
+    pass.setInputCloud (input_cloud);
+    pass.setFilterFieldName ("z");
+    pass.setFilterLimits (1.0, 500);
+    //pass.setFilterLimitsNegative (true);
+    pass.filter (*cloud_passthrough);
+    // cout << "after z filter  " << cloud_passthrough->points.size() << endl;
+
+    // pass.setInputCloud (cloud_passthrough);
+    // pass.setFilterFieldName ("x");
+    // pass.setFilterLimits (x_min, x_max);
+    // pass.filter (*cloud_passthrough);
+    // // cout << "after x filter  " << cloud_passthrough->points.size() << endl;
+
+    // pass.setInputCloud (cloud_passthrough);
+    // pass.setFilterFieldName ("y");
+    // pass.setFilterLimits (y_min, y_max);
+    // pass.filter (*cloud_passthrough);
+    // // cout << "after y filter  " << cloud_passthrough->points.size() << endl;
+
+    return *cloud_passthrough; 
+} 
+
+
+Mat label_path(const sensor_msgs::ImageConstPtr& image_msg)
+{
+    // init output image 
+    Mat image_raw, labeled_img; 
+    // try { 
+    //   image_raw = cv_bridge::toCvShare(image_msg, "bgr8")->image;
+    // }
+    // catch (cv_bridge::Exception& ex){  
+    //   cout << ex.what() << endl;
+    // }
+    // labeled_img = Mat(labeled_img.rows, labeled_img.cols, CV_8UC1, Scalar(0));
+
+    // convert pointcloud   
+    pcl::PointCloud<pcl::PointXYZRGB> pcl_cloud, path_camera, cloud_camera, filtered_world;
+
+    sensor_msgs::PointCloud2 cloud_transformed = transform_cloud(*registered_cloud_, world_frame_);
+    pcl::fromROSMsg(cloud_transformed, pcl_cloud); 
+
+    int start = transforms_to_camera_.size() - 10;
+    if(start < 0)
+        start = 0;
+
+    for(int i = start; i < transforms_to_camera_.size(); i++)
+    // for(int i = 0; i < 1; i++)
+    {   
+        Eigen::Matrix4f transform_to_camera = transforms_to_camera_[i];
+        Eigen::Matrix4f transforms_to_world = transforms_to_world_[i];
+        pcl::transformPointCloud (pcl_cloud, cloud_camera, transform_to_camera);
+        pcl::transformPointCloud (robot_path_, path_camera, transform_to_camera);
+
+        cloud_camera = cloud_filter(cloud_camera);
+
+        ci_mapper->get_disparity(raw_images_[i], camera_frame_, cloud_camera);
+        path_camera = ci_mapper->get_visiable_path(raw_images_[i], path_camera);
+
+        pcl::PointCloud<pcl::PointXYZRGB> path_world;
+        pcl::transformPointCloud (path_camera, path_world, transforms_to_world);
+        path_world.header.frame_id = world_frame_;
+        publish(pub_path, path_world);
+
+        // imshow("image_raw", raw_images_[i]);
+        // waitKey(100);
+
+        break;
+    }
+
+
+    return labeled_img;
+}
+
+
+void imageCallback_raw(sensor_msgs::ImageConstPtr image_msg)
+{
+    // start processing
+    // cout << "raw image recieved: " <<image_msg->header.frame_id << endl;
+    camera_frame_ = image_msg->header.frame_id;
+    if(count_ > 11)
+    {
+        label_path(image_msg);
+        robot_path_.header.frame_id = world_frame_;
+        // publish(pub_cloud, robot_path_);
+    }
+    // else
+    {
+        // get camera position in world frame and get transform from world to camera frame
+        tf::StampedTransform base_to_world, world_to_camera, camera_to_world;
+        try 
+        {
+            tfListener->waitForTransform(world_frame_, image_msg->header.frame_id, image_msg->header.stamp, ros::Duration(0.1));
+            tfListener->lookupTransform(world_frame_, base_frame_,  image_msg->header.stamp, base_to_world);
+            tfListener->lookupTransform(image_msg->header.frame_id, world_frame_, image_msg->header.stamp, world_to_camera);
+            tfListener->lookupTransform(world_frame_, image_msg->header.frame_id, image_msg->header.stamp, camera_to_world);
+        }
+        catch (tf::TransformException& ex) 
+        {
+            ROS_WARN("[draw_frames] TF exception:\n%s", ex.what());
+            return;
+        }   
+ 
+        cout << "raw image recieved: " << robot_path_.points.size() << endl;
+        // save raw rgb image 
+        try {
+            Mat image_raw = cv_bridge::toCvShare(image_msg, "bgr8")->image;
+            raw_images_.push_back(image_raw.clone());
+
+            // imshow("image", image_raw);
+            // imshow("image_raw", raw_images_[0]);
+            // waitKey(100);
+        }
+        catch (cv_bridge::Exception& ex){
+            cout << ex.what() << endl;
+            return; 
+        } 
+
+        // save path point and transforms
+        pcl::PointXYZRGB camera_position; 
+        camera_position.x = base_to_world.getOrigin().x(); 
+        camera_position.y = base_to_world.getOrigin().y();
+        camera_position.z = base_to_world.getOrigin().z() + 0.1;
+
+        cout << camera_position.x << " " << camera_position.y << " " << camera_position.z << endl;
+        robot_path_.points.push_back(camera_position);
+
+        Eigen::Matrix4f eigen_transform, eigen_transform_toworld; 
+        pcl_ros::transformAsMatrix (world_to_camera, eigen_transform);
+        pcl_ros::transformAsMatrix (camera_to_world, eigen_transform_toworld);
+
+        transforms_to_camera_.push_back(eigen_transform);
+        transforms_to_world_.push_back(eigen_transform_toworld);
+
+        robot_path_.header.frame_id = world_frame_;
+        // publish(pub_cloud, robot_path_);
+
+        count_ ++;
+    }
+}
+
+void callback_velodyne(const sensor_msgs::PointCloud2ConstPtr &cloud_in)
+{
+    registered_cloud_ = cloud_in;
+    input_frame_ = cloud_in->header.frame_id;
+}
+ 
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "self_labeller");
+
+    ros::NodeHandle node;  
+    tfListener      = new (tf::TransformListener);
+    ci_mapper       = new Cloud_Image_Mapper(tfListener);
+
+    // robot_path_ = pcl::PointCloud<pcl::PointXYZRGB>();
+
+    ros::Subscriber sub_cloud   = node.subscribe<sensor_msgs::PointCloud2>("/points_raw", 1, callback_velodyne);
+    pub_cloud                   = node.advertise<sensor_msgs::PointCloud2>("/cloud_filtered", 1);
+    pub_path                   = node.advertise<sensor_msgs::PointCloud2>("/robot_path", 1);
+    //image_transport::Publisher pub = it.advertise("camera/image", 1);
+    
+    image_transport::ImageTransport it(node);
+    // image_transport::Subscriber sub_raw = it.subscribe("/image_raw", 1, imageCallback_raw);
+
+    pub_img    = it.advertise("/self_label/raw", 1);
+
+    int skip_msg_count = 0; 
+
+    ros::Rate loop_rate(3);
+    while (ros::ok()) 
+    {
+        sensor_msgs::ImageConstPtr msg = ros::topic::waitForMessage<sensor_msgs::Image>("/image_raw", ros::Duration(10));
+        if(skip_msg_count < 5)
+        {
+            skip_msg_count ++;
+        }
+        else
+        {
+            imageCallback_raw(msg);
+        }
+        
+        ros::spinOnce();
+        loop_rate.sleep();
+    }
+    ros::spinOnce();
+
+    return 0;
+}
+
---
+++ src/self_labeller.h
@@ -0,0 +1,245 @@
+#include <image_transport/image_transport.h>
+#include <image_geometry/pinhole_camera_model.h>
+#include <tf/transform_listener.h>
+#include <boost/foreach.hpp>
+#include <sensor_msgs/image_encodings.h>
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+#include <cv_bridge/cv_bridge.h>
+
+#include <sensor_msgs/PointCloud2.h>
+#include <sensor_msgs/point_cloud_conversion.h>
+#include <pcl_ros/transforms.h>
+#include <pcl_conversions/pcl_conversions.h>
+
+#include <pcl/filters/passthrough.h>
+
+using namespace std;
+using namespace cv;
+
+#define FLAT    0
+#define ROUGH   100 
+#define OBS     200
+
+float f = 2262.52;
+float base_line = 0.209313;
+float scale = f*base_line/2.0;
+
+class Cloud_Image_Mapper
+{
+  tf::TransformListener *tf_listener_;
+  // image_geometry::PinholeCameraModel cam_model_;
+  CvFont font_;
+
+public:
+  ros::Time cloud_in_time_;
+
+  Mat img_ori, image_label;
+  Mat img_seg_, img_path_, img_fused_, img_depth_, img_all_, img_rgb_;
+
+  pcl::PointCloud<pcl::PointXYZRGB> cloud_g;
+  pcl::PointCloud<pcl::PointXYZRGB> cloud_v;
+  pcl::PointCloud<pcl::PointXYZRGB> cloud_f;
+
+  Cloud_Image_Mapper(tf::TransformListener *tf_listener)
+  {
+    tf_listener_ = tf_listener;
+  }
+
+  cv::Point2d project3D_to_image(cv::Point3d& xyz, string frame_id )
+  {
+    double fx, fy, cx, cy; 
+    if(frame_id == "kinect2_rgb_optical_frame")
+    {
+      fx = 529.9732789120519;
+      fy = 526.9663404399863;
+      cx = 477.4416333879422;
+      cy = 261.8692914553029;
+    }
+    else
+    {
+      fx = 775.3905399535238;
+      fy = 775.3925549639409;
+      cx = 651.1391917338947;
+      cy = 394.3686338123942;
+    }
+    cv::Point2d uv_rect;
+    uv_rect.x = (fx*xyz.x) / xyz.z + cx;
+    uv_rect.y = (fy*xyz.y) / xyz.z + cy;
+    return uv_rect;
+  }
+
+  void set_point_color(pcl::PointXYZRGB &point, int label)
+  {
+    if(label == FLAT)
+    {
+      point.r = 0;
+      point.g = 0;
+      point.b = 255;
+    }
+    else if(label == ROUGH)
+    {
+      point.r = 255;
+      point.g = 255;
+      point.b = 0;
+    }
+    else if(label == OBS)
+    {
+      point.r = 255;
+      point.g = 0;
+      point.b = 0;
+    }      
+  }
+
+  Scalar get_pixel_color(int label)
+  {
+    Scalar pix_color;
+    if(label == FLAT)
+    {
+      pix_color.val[2] = 0;
+      pix_color.val[1] = 0;
+      pix_color.val[0] = 255;
+    }
+    else if(label == ROUGH)
+    {
+      pix_color.val[2] = 255;
+      pix_color.val[1] = 255;
+      pix_color.val[0] = 0;
+    }
+    else if(label == OBS)
+    {
+      pix_color.val[2] = 255;
+      pix_color.val[1] = 0;
+      pix_color.val[0] = 0;
+    }      
+
+    return pix_color;
+  }
+
+  pcl::PointCloud<pcl::PointXYZRGB> get_visiable_path(Mat image_raw, pcl::PointCloud<pcl::PointXYZRGB> cloud_path)
+  {
+    // img_path_ = Mat(img_depth_.rows, img_depth_.cols, CV_8UC1, Scalar(0));
+    img_path_ = image_raw.clone();
+    int count_invisable = 0;
+    for(int i = 0; i < cloud_path.points.size(); i++)
+    {
+      pcl::PointXYZRGB path_point = cloud_path.points[i];
+      // if(path_point.z > 5)
+      //   continue;
+
+      cv::Point3d pt_cv(path_point.x, path_point.y, path_point.z);
+
+      cv::Point2d uv;
+      uv = project3D_to_image(pt_cv, "kinect2_rgb_optical_frame");
+
+      int boundary = 15;
+      if(uv.x >= boundary && uv.x < img_depth_.cols - boundary && uv.y >= boundary && uv.y < img_depth_.rows - boundary)
+      {
+        unsigned short depth_before = img_depth_.at<unsigned short>(uv.y, uv.x);
+
+        cv::Point3d pt_cv2(path_point.x + 0.30, path_point.y, path_point.z);
+        cv::Point2d uv2;
+        uv2 = project3D_to_image(pt_cv2, "kinect2_rgb_optical_frame");
+
+        int radius = abs(uv2.x - uv.x);
+
+        unsigned short disparity = scale/path_point.z * 256 + 1;
+        cout << disparity << " depth image: " << (int)depth_before << " " << disparity - depth_before << endl;
+
+        if(depth_before > disparity)
+        {
+          cloud_path.points[i].r = 255;
+          // count_invisable ++;
+          // cv::circle(img_path_, uv, radius, Scalar(0, 0, 255), -1);  
+        }
+        else
+        {
+          cloud_path.points[i].b = 255;
+          cout << "radius: " << radius << endl;
+          cv::circle(img_path_, uv, radius, Scalar(255, 0, 0), -1);  
+        }
+      }
+    }
+    cout << "occluded path points: " << count_invisable << endl;
+    
+    imshow("img_path_", img_path_);
+    waitKey(50);
+
+    return cloud_path;
+  }
+
+  Mat get_disparity(Mat image_raw, string frame_id, pcl::PointCloud<pcl::PointXYZRGB> pcl_cloud_filtered)
+  {   
+    // init output image
+    if(pcl_cloud_filtered.points.size() == 0)
+      return image_raw;
+    
+    img_rgb_ = image_raw.clone();
+
+    string image_frame_id;
+    if(frame_id == "kinect2_rgb_optical_frame")
+      image_frame_id  = frame_id;
+    else 
+      image_frame_id  = "overhead_camera_link";
+
+    img_depth_ = Mat(image_raw.rows, image_raw.cols, CV_16UC4, Scalar(0));
+    // img_all_   = Mat(image_raw.rows, image_raw.cols, CV_16UC4, Scalar(0));
+    // Mat img_depth_display_   = Mat(image_raw.rows, image_raw.cols, CV_16UC1, Scalar(0));
+
+    for(int i = 0; i < pcl_cloud_filtered.points.size(); i++)
+    {
+      pcl::PointXYZRGB point_transd = pcl_cloud_filtered.points[i];
+      // if(point_transd.z < 0 || abs(point_transd.x) > 6)
+      //   continue;
+      
+      cv::Point3d pt_cv(point_transd.x, point_transd.y, point_transd.z);
+      
+      cv::Point2d uv;
+      uv = project3D_to_image(pt_cv, image_frame_id);
+
+      static const int RADIUS = 7;
+      
+      if(uv.x >= 0 && uv.x < image_raw.cols && uv.y >= 0 && uv.y < image_raw.rows)
+      {
+        // float depth = point_transd.z;
+        unsigned short depth = scale/point_transd.z * 256 + 1;
+
+        unsigned short depth_before = img_depth_.at<unsigned short>(uv.y, uv.x);
+
+        if(depth_before < depth)
+          // img_depth_.at<float>(uv.y, uv.x) = depth;
+          cv::circle(img_depth_, uv, 5, Scalar(depth, depth, depth), -1);  
+      }
+    } 
+
+    Mat element = getStructuringElement(MORPH_RECT, Size(3,3));
+    morphologyEx(img_depth_, img_depth_, MORPH_DILATE, element);
+    cout << "got depth image" << img_depth_.rows << " " << img_depth_.cols << endl;
+
+    // for(int row = 0; row < image_raw.rows; row ++)
+    // {
+    //   for(int col = 0; col < image_raw.cols; col ++)
+    //   {
+    //     Vec4s u4_value;
+    //     Vec3b raw_color = image_raw.ptr<Vec3b>(row)[col];
+    //     unsigned short depth = img_depth_.ptr<unsigned short>(row)[col];
+
+    //     u4_value.val[0] = (unsigned short) (raw_color.val[0]);
+    //     u4_value.val[1] = (unsigned short) (raw_color.val[1]);
+    //     u4_value.val[2] = (unsigned short) (raw_color.val[2]);
+    //     u4_value.val[3] = depth;
+
+    //     img_all_.ptr<Vec4s>(row)[col] = u4_value;
+    //     img_depth_display_.ptr<unsigned short>(row)[col] = depth;
+    //   }
+    // }
+    // cout << "got all image " << img_all_.rows << " " << img_all_.cols << endl;
+    imshow("depth", img_depth_);
+    // waitKey(0);
+    return img_depth_;  
+  }
+};
+
